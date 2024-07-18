2f264fc043fd8ff396e6cde4db3513d19395c20d
Lusapho Matiti <lusaphomatiti07@gmail.com>
2024-07-18 13:00:00 +0200

    Updated funtionality

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export const homePageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
   { id: 3, href: "#services", text: "services" },
-  { id: 4, href: "#tours", text: "projects" },
+  { id: 4, href: "#projects", text: "projects" },
 ];
 
 export const otherPageLinks = [
